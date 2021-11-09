8730b608a6371c61c8f06039d9d9795d83bee7d0
Jess Martin <jessmartin@gmail.com>
2021-11-08 21:56:03 -0500

    Initial working version of dragging cards

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import "@lourenci/react-kanban/dist/styles.css";
 // Use your own styles to override the default styles
 // import "./styles.css";
 
+import { App as CroquetApp, Model } from "@croquet/croquet";
+import {
+  usePublish,
+  useModelRoot,
+  InCroquetSession,
+  useSubscribe,
+  CroquetContext
+} from "@croquet/react";
+
 const board = {
   columns: [
     {
@@ -13,74 +22,54 @@ const board = {
       cards: [
         {
           id: 1,
-          title: "Card title 1",
-          description: "Card content"
-        },
-        {
-          id: 2,
-          title: "Card title 2",
-          description: "Card content"
-        },
-        {
-          id: 3,
-          title: "Card title 3",
-          description: "Card content"
+          title: "Card 1",
+          description: "This is a card",
         }
       ]
     },
     {
       id: 2,
-      title: "Doing",
+      title: "Backlog",
       cards: [
-        {
-          id: 9,
-          title: "Card title 9",
-          description: "Card content"
-        }
-      ]
-    },
-    {
-      id: 3,
-      title: "Q&A",
-      cards: [
-        {
-          id: 10,
-          title: "Card title 10",
-          description: "Card content"
-        },
-        {
-          id: 11,
-          title: "Card title 11",
-          description: "Card content"
-        }
-      ]
-    },
-    {
-      id: 4,
-      title: "Production",
-      cards: [
-        {
-          id: 12,
-          title: "Card title 12",
-          description: "Card content"
-        },
-        {
-          id: 13,
-          title: "Card title 13",
-          description: "Card content"
-        }
       ]
     }
   ]
 };
 
+class KanbanModel extends Model {
+  init(option) {
+    super.init(option);
+    this.board = board;
+    this.subscribe(this.id, "dragCard", this.onDragCard);
+  }
+
+  onDragCard({ source, destination }) {
+    const updatedBoard = moveCard(this.board, source, destination);
+    this.board = updatedBoard;
+    this.publish(this.id, "updateBoard");
+    console.log(this.board);
+  }
+}
+KanbanModel.register("KanbanModel");
+
 function ControlledBoard() {
   // You need to control the state yourself.
-  const [controlledBoard, setBoard] = useState(board);
+  const model = useModelRoot();
+  const [controlledBoard, setBoard] = useState(model.board);
+
+  const publishCardDrag = usePublish((source, destination) => [model.id, "dragCard", { source, destination }]);
+  useSubscribe(model.id, "updateBoard", updateBoard);
+
+  function updateBoard() {
+    console.log(model.board);
+    setBoard(model.board);
+  }
 
   function handleCardMove(_card, source, destination) {
     const updatedBoard = moveCard(controlledBoard, source, destination);
     setBoard(updatedBoard);
+    publishCardDrag(source, destination);
+    // console.log(updatedBoard)
   }
 
   return (
@@ -90,39 +79,17 @@ function ControlledBoard() {
   );
 }
 
-function UncontrolledBoard() {
-  return (
-    <Board
-      allowRemoveLane
-      allowRenameColumn
-      allowRemoveCard
-      onLaneRemove={console.log}
-      onCardRemove={console.log}
-      onLaneRename={console.log}
-      initialBoard={board}
-      allowAddCard={{ on: "top" }}
-      onNewCardConfirm={draftCard => ({
-        id: new Date().getTime(),
-        ...draftCard
-      })}
-      onCardNew={console.log}
-    />
-  );
-}
-
 function App() {
   return (
-    <>
-      <h4>Example of an uncontrolled board</h4>
-      <UncontrolledBoard />
-      <h4>Example of a controlled board</h4>
-      <p>Just the card moving is implemented in this demo.</p>
-      <p>
-        In this kind of board, you can do whatever you want. We just mirror your
-        board state.
-      </p>
+    <InCroquetSession
+      apiKey="18fIEcbGu1vlEAc4bnppPMtIccJ5xIn6J1dJGMeay"
+      appId="in.jessmart.react.kanban"
+      password={CroquetApp.autoPassword()}
+      name={CroquetApp.autoSession()}
+      model={KanbanModel}
+    >
       <ControlledBoard />
-    </>
+    </InCroquetSession>
   );
 }
 
